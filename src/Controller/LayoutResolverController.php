eb6fdd631d73c14e5f014f246fd92fbced9061dc
Matt Owens <mowens3@gmail.com>
2026-01-03 17:59:59 -0800

    fix: use application/json for layout resolver

--- src/Controller/LayoutResolverController.php
+++ src/Controller/LayoutResolverController.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpFoundation\Request;
  */
 final class LayoutResolverController extends ControllerBase {
 
-  private const CONTENT_TYPE = 'application/vnd.api+json; charset=utf-8';
+  private const CONTENT_TYPE = 'application/json; charset=utf-8';
 
   public function __construct(
     private readonly PathResolverInterface $resolver,
